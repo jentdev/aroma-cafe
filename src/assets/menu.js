e1adb63c73e0fbdaa423621667ad60b0af82c520
Jennie Nguyen <jentdev@gmail.com>
2025-03-26 17:30:26 -0500

    added menu

---
+++ src/assets/menu.js
@@ -0,0 +1,110 @@
+export const espresso = [
+    {
+        drink: espresso,
+        price: [{hot: 6}]
+    },
+    {
+        drink: americano,
+        price: [{hot: 7.50, iced: 7.25}]
+    },
+    {
+        drink: cappuccino,
+        price: [{hot: 7.75, iced: 7.50}]
+    },
+    {
+        drink: 'mocha latte',
+        price: [{hot: 8, iced: 7.75}]
+    },
+    {
+        drink: latte,
+        price: [{hot: 6.25, iced: 6}]
+    },
+];
+
+export const coldBrew = [
+    {
+        drink: 'cold brew',
+        price: [{'16oz': 6.75, '20oz': 7.50}]
+    },
+    {
+        drink: 'aroma cold brew',
+        price: [{'16oz': 8, '20oz': 8.75}]
+    },
+    {
+        drink: 'New York cold brew',
+        price: [{'16oz': 7.75, '20oz': 7}]
+    },
+];
+
+export const latte = [
+    {
+        drink: 'crème brûlée latte',
+        price: [{'12oz': 8.50}]
+    },
+    {
+        drink: 'tiramisu latte',
+        price: [{'16oz': 9}]
+    },
+    {
+        drink: 'einspänner latte',
+        price: [{'8oz': 7.25}]
+    },
+];
+
+export const tea = [
+    {
+        drink: 'chai tea',
+        price: [{hot: 7.50, iced: 7.25}]
+    },
+    {
+        drink: 'chamomile tea',
+        price: [{hot: 6.25}]
+    },
+    {
+        drink: 'earl grey tea',
+        price: [{hot: 6.25}]
+    },
+    {
+        drink: 'peach peony tea',
+        price: [{iced: 6.25}]
+    },
+    {
+        drink: 'einspänner matcha',
+        price: [{iced: 7.50}]
+    },
+    {
+        drink: 'ceremonial matcha',
+        price: [{hot: 7.25, iced: 7}]
+    },
+];
+
+export const pastries = [
+    {
+        pastry: 'apply rose pie',
+        price: 6
+    },
+    {
+        pastry: 'raisin cinnamon roll',
+        price: 6.25
+    },
+    {
+        pastry: 'almond croissant',
+        price: 7
+    },
+    {
+        pastry: 'chocolate eclair',
+        price: 6
+    },
+    {
+        pastry: 'fruit tart',
+        price: 7.25
+    },
+    {
+        pastry: 'apply rose pie',
+        price: 6
+    },
+    {
+        pastry: 'cheese danish',
+        price: 5.75
+    },
+];
